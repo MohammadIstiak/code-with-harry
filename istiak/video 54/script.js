e8ff31b085fa9873d155ac6f90d7155acc2e8a05
Mohammad Istiak Azam <146804572+MohammadIstiak@users.noreply.github.com>
2024-05-17 05:09:43 +0600

    Add files via upload

---
+++ istiak/video 54/script.js
@@ -0,0 +1,15 @@
+alert("hello world")
+console.log("code is running...")
+var a = prompt("enter your number")
+
+var istrue = confirm("are you sure you want to leave this page and blast your computer")
+if(istrue){
+    console.log("computer is blasting ")
+    
+}
+else{
+    console.log("computer is not blasting")
+}
+console.log("your number is " + a);
+document.title="heyi am good"
+document.body.style.backgroundColor="green"
---
+++ istiak/video 55/script.js
@@ -0,0 +1,29 @@
+console.log("hey this is tutorial 55 of js")
+
+
+
+let a=5;
+a =a+1
+let b=6;
+let c="harry";
+let _a="shuvam"
+// let 55a="rohan"not allowed
+
+
+console.log(a+b+8)
+console.log(typeof a,typeof b,typeof c,typeof _a)
+
+// const a1=6;
+// a1=a1+1 not allowed because a1 is constant
+
+
+
+
+
+
+
+
+
+
+
+
